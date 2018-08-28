0e4662096d9199c3ad2029c02cf3444cdfcd0f7d
Halljamie18 <jamie.hall@blis.com>
2018-08-28 19:17:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ function fetchBooks() {
   //write fetch request to the Game of Thrones API
 }
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks(){
     fetch('https://anapioficeandfire.com/api/books')
@@ -19,6 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 })
 
-  renderBooks()
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
 
 
